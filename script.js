d14bf739c23c01c64b22473f0793754531ec1c5a
johney <johneyburke@gmail.com>
2023-08-23 08:21:24 -0600

    added js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
